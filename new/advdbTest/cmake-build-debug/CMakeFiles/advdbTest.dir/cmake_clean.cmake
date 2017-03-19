f3b6f16c334de20f6300911aef6c52cf4c0c60e4
sdhayalk <sdhayalk@asu.edu>
2017-03-19 03:33:00 -0700

    test cases 2

---
+++ new/advdbTest/cmake-build-debug/CMakeFiles/advdbTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/advdbTest.dir/main.cpp.o"
+  "advdbTest.pdb"
+  "advdbTest"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/advdbTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
