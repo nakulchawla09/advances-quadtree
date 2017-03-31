36f734b83f41f430e6fa24ef603b5eb99117d5cb
nakulchawla09 <nakulchawla09@gmail.com>
2017-03-30 17:34:19 -0700

    Add bounding box coordinates

--- new/advdbSpatialTest/UtilityFunctions.h
+++ new/advdbSpatialTest/UtilityFunctions.h
@@ -17,5 +17,38 @@ void printQPointVectors(vector<QPoint> qP)
     }
 }
 
+void getBoundingBoxCooridinates(vector<QPoint> qP)
+{
+    float minX,maxX,minY,maxY,currentX,currentY; int i;
+    minX = qP[1].getX();
+    minY = qP[1].getY();
+    maxX = qP[1].getX();
+    maxY = qP[1].getY();
+
+    for(i=0;i< qP.size();i++){
+        currentX = qP[i].getX();
+        currentY = qP[i].getY();
+        if(currentX <= minX){
+            minX = currentX;
+        }else if (currentX >= maxX){
+            maxX = currentX;
+        }
+
+        if(currentY <= minY){
+            minY = currentY;
+        }else if(currentY >= maxY){
+            maxY = currentY;
+        }
+    }
+
+    //coordinates for bounding box should be (minX,minY) , (maxX,maxY)
+
+}
+
+//void getHeightAndWidth(vector<QPoint> qP){
+//
+//
+//}
+
 
 #endif //ADVDBTEST_UTILITYFUNCTIONS_H
